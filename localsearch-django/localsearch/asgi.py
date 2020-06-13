f8c77f2778d5e2edfa813bcc76373b23deb6ca84
gwj0012 <garrettjennings@my.unt.edy>
2020-06-13 00:53:37 -0500

    moved files

--- localsearch-django/localsearch/asgi.py
+++ localsearch-django/localsearch/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for localsearch project.
+ASGI config for localsearch-django project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localsearch.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localsearch-django.settings')
 
 application = get_asgi_application()
--- localsearch-django/manage.py
+++ localsearch-django/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localsearch.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'localsearch-django.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
