90d4ebb974cd833a2e96d5e82459b35ed4063bbe
gwj0012 <garrettwjennings@gmail.com>
2020-06-28 22:21:01 -0500

    database settings

---
+++ searchengine/searchenginein/test.py
@@ -0,0 +1,13 @@
+
+import pymysql
+
+db=pymysql.connect('127.0.0.1', 'root', 'python5717', 'mydb')
+
+cursor = db.cursor()
+cursor.execute("select version()")
+data = cursor.fetchone()
+
+print(data)
+
+cursor.close()
+db.close()
