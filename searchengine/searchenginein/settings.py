86a9d8d2178c20a40773a879bd3b53bfa53f3ef9
gwj0012 <garrettwjennings@gmail.com>
2020-06-28 23:08:27 -0500

    commit

--- searchengine/searchenginein/settings.py
+++ searchengine/searchenginein/settings.py
@@ -106,7 +106,7 @@ WSGI_APPLICATION = 'searchengine.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database',
+        'NAME': 'pydb',
         'USER' : 'root',
         'PASSWORD' : 'python5717',
         'HOST' : '127.0.0.1',
