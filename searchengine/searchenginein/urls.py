d20538c7357b4a79dfb7e5d3f350135e8adbb3eb
gwj0012 <garrettjennings@my.unt.edy>
2020-07-20 03:53:53 -0500

    trying to fix manage issues

--- searchengine/searchenginein/urls.py
+++ searchengine/searchenginein/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from usercontrol.views import userview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('usercontrol/login', userview.LoginView),
 ]
