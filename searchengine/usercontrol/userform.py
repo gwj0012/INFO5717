0161ff9751b24ef1cc44258090dcfaf1e42bd26b
gwj0012 <garrettjennings@my.unt.edy>
2020-07-29 05:16:54 -0500

    more classes

--- searchengine/usercontrol/userform.py
+++ searchengine/usercontrol/userform.py
@@ -13,4 +13,4 @@ class NewPass(forms.Form):
 class Recovery(forms.Form):
     username = forms.CharField(label='username', max_length=100, required=True)
     security = forms.CharField(label='security', max_length=150, required=True)
-    answer = forms.CharField(label='answer', max_length==100, required=True)
+    answer = forms.CharField(label='answer', max_length=100, required=True)
