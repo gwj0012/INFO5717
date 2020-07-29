bbd0394c2a6e64c15eba2fa5fec155542c0d5b96
gwj0012 <garrettjennings@my.unt.edy>
2020-07-29 05:04:47 -0500

    login action is finalized

--- searchengine/usercontrol/views.py
+++ searchengine/usercontrol/views.py
@@ -5,7 +5,29 @@ from usercontrol.userform import UserForm
 # Create your views here.
 
 def LoginView(request):
-    return render(request, 'login.html')
+    userform = UserForm()
+    context = {'userform':userform}
+    return render(request, 'login.html', context)
+
+def LoginAction(request):
+    if request.method == "POST":
+        form=UserForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = TUser.objects.filter(username=username, password=password)
+            print(userid)
+            if len(user) > 0 :
+                request.session['username'] = user[0].username
+                request.session['password'] = user[0].password
+                return render(request, 'textmanagement.html')
+            else:
+                return HttpResponseRedirect('/usercontrol/login')
+        else:
+            return HttpResponseRedirect('/usercontrol/login')
+    else:
+        return HttpResponseRedirect('/usercontrol/login')
 
 def NewPassword(request):
     return render(request, 'newpasssword.html')
@@ -16,4 +38,4 @@ def Password(request):
 def Logout(request):
     del request.session['username']
     del request.session['password']
-    return HttpResponseRedirect('/usercontrol/login.html')
+    return HttpResponseRedirect('/usercontrol/login')
